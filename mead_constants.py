3d282ecbd31b35877cbce5c1c58d3658d0599ce0
Alexander Mead <alexander.j.mead@googlemail.com>
2022-05-20 13:00:09 -0700

    tidy

--- mead_constants.py
+++ mead_constants.py
@@ -1,29 +1,33 @@
 # Try https://docs.scipy.org/doc/scipy/reference/constants.html
 
 # Physical constants
-h = 6.62607004e-34 # Planck constant [kg m^2 s^-1]
-c = 2.998e8 # Speed of light [m s^-1]
+# TODO: Take from from scipy.constants
+G = 6.6743e-11      # Gravitational constant [kg^-1 m^3 s^-2]
+h = 6.62607004e-34  # Planck constant [kg m^2 s^-1]
+c = 2.998e8         # Speed of light [m s^-1]
 kB = 1.38064852e-23 # Boltzmann constant [kg m^2 s^-2 K^-1]
-mp = 1.6726219e-27 # Proton mass [kg]
+mp = 1.6726219e-27  # Proton mass [kg]
 
 # Units
-Jy = 1e-26 # Jansky [W m^-2 Hz^-1]
-au = 1.496e11 # au [m]
-eV_mass = 1.79e-36 # eV/c^2 [kg]
+Jy = 1e-26                # Jansky [W m^-2 Hz^-1]
+au = 1.496e11             # au [m]
+eV_mass = 1.79e-36        # eV/c^2 [kg]
 year = 365.25*24.*60.*60. # year [s]
 
 # Time
-seconds_in_day = 60*60*24
+seconds_in_day = 60*60*24 # [s]
 
 # Astronomy
-R_Sun = 6.957e8 # Solar radius [m]
-M_Sun = 2.e30 # Solar mass [kg]
+Sun_radius = 6.96340e8  # Radius of the Earth [m]
+Sun_mass = 1.9884e30    # Mass of the Sun [kg]
+Earth_mass = 5.9722e24  # Mass of the Earth [kg]
+Jupiter_mass = 1.898e27 # Mass of Jupiter [kg]
 
 # Cosmology
 # TODO: Derive those that should be derived
-Hdist = 2997.92458 # c/H0 [Mpc/h]
+Hdist = 2997.92458  # c/H0 [Mpc/h]
 Htime = 9.777922217 # 1/H0 [Gyr/h]
-H0 = 100. # H0 in h [km/s/Mpc]
+H0 = 100.           # H0 in h [km/s/Mpc]
 #rhoc = 277537383080.52332 # Critical density [(Msun/h) (Mpc/h)^-3]
-rhoc = 2.775373e11 # Critical density [(Msun/h) (Mpc/h)^-3]
-nuconst = 93.1 # Neutrino mass required to close universe [eV]
+rhoc = 2.775373e11  # Critical density [(Msun/h) (Mpc/h)^-3]
+nuconst = 93.1      # Neutrino mass required to close universe [eV]
