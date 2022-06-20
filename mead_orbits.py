1f9d4b6ddee2ff84e76055eba40ff60145eb1769
Alexander Mead <alexander.j.mead@googlemail.com>
2022-06-20 16:33:57 -0700

    mean and true anomaly (Kepler equation)

---
+++ mead_orbits.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.optimize import fsolve
+from scipy.integrate import quad
+
+def mean_anomaly(nu, e, approximate=False):
+    '''
+    Compute the mean anomaly corresponding to the true anomaly via integration
+    '''
+    if approximate: # Taylor expansion for low e
+        result = nu-2.*e*np.sin(nu)
+    else:
+        integrand = lambda x: 1./(1.+e*np.cos(x))**2
+        integral, _ = quad(integrand, 0., nu)
+        result = ((1.-e**2)**1.5)*integral
+    return result
+
+def true_anomaly(M, e, approximate=False):
+    '''
+    Compute the true anomaly from the mean anomaly using root finding
+    '''
+    f = lambda nu: mean_anomaly(nu, e, approximate)-M
+    nu = fsolve(f, M) # Initial guess that nu = M
+    return nu[0]
