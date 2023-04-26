345de2fc0b2b3df08317df3be97ba3cc0269f93c
Alexander Mead <alexander@digilab.co.uk>
2023-04-26 11:32:54 +0100

    typehints; pad_list; pep8

--- mead_general.py
+++ mead_general.py
@@ -4,8 +4,10 @@ import numpy as np
 
 ### High level ###
 
+
 def module_info(mod):
     print(mod.__name__, mod.__version__, mod.__file__)
+
 
 def none_or_string(value):
     if value == 'None':
@@ -18,19 +20,22 @@ def none_or_string(value):
 
 ### This of functions use only basic Python ###
 
+
 def periodic_integer(i, n):
     '''
     Ensure that integers can only exist on the range [0, 1, ..., n-1]
     Solution from: https://stackoverflow.com/questions/43827464
     '''
-    return ((i%n)+n)%n
+    return ((i % n)+n) % n
+
 
 def periodic_float(x, L):
     '''
     Ensures that the float x can only exist on the interval [0, L)
     NOTE: This is just the modulus operation, but I always forget this
     '''
-    return x%L
+    return x % L
+
 
 def opposite_side(left_or_right):
     '''
@@ -43,6 +48,7 @@ def opposite_side(left_or_right):
     else:
         raise ValueError('Input should be either \'left\' or \'right\'')
     return out
+
 
 def number_name(n):
     '''
@@ -67,6 +73,7 @@ def number_name(n):
     else:
         raise ValueError('Integer does not appear to have a name')
 
+
 def file_length(fname):
     '''
     Count the number of lines in a file
@@ -77,6 +84,7 @@ def file_length(fname):
         for i, _ in enumerate(f):
             pass
     return i+1
+
 
 def mrange(a, b=None, step=None):
     '''
@@ -95,18 +103,21 @@ def mrange(a, b=None, step=None):
             return range(a, b+1)
     else:
         if b is None:
-            raise ValueError('If a step is specified then you must also specify start (a) and stop (b)')
+            raise ValueError(
+                'If a step is specified then you must also specify start (a) and stop (b)')
         else:
             return range(a, b+1, step)
 
-def is_float_close_to_integer(x):
+
+def is_float_close_to_integer(x: float) -> bool:
     '''
     Checks if float is close to an integer value
     '''
     from math import isclose
     return isclose(x, int(x))
 
-def bin_edges_for_integers(a, b=None):
+
+def bin_edges_for_integers(a: int, b=None) -> list:
     '''
     Defines a set of bin edges for histograms of integer data
     e.g., bin_edges_for_integers(1, 5) = 0.5, 1.5, 2.5, 3.5, 4.5, 5.5 so that values are at centres
@@ -116,10 +127,11 @@ def bin_edges_for_integers(a, b=None):
         bin_edges = list(mrange(min(a), max(a)+1))
     else:
         bin_edges = list(mrange(a, b+1))
-    bin_edges = [bin_edge-0.5 for bin_edge in bin_edges] # Centre on integers
+    bin_edges = [bin_edge-0.5 for bin_edge in bin_edges]  # Centre on integers
     return bin_edges
 
-def key_from_value(dict, value):
+
+def key_from_value(dict: dict, value: any) -> str:
     '''
     Get a dictionary key from the value
     From: https://stackoverflow.com/questions/8023306
@@ -131,24 +143,40 @@ def key_from_value(dict, value):
 
 ### These functions operate on collections (lists, tuples, sets, dictionaries) ###
 
-def count_entries_of_nested_list(listOfElem):
-    ''' Get number of elements in a nested list'''
+
+def pad_list(a: list, n: int) -> list:
+    '''
+    If a list contains fewer than n elements then expand and pad it to be length n
+    by prepending the list using the 0-th element until it is n long
+    TODO: Could also pad by appending the last element 0 -> -1?
+    '''
+    while len(a) < n:
+        a.insert(0, a[0])
+    return a
+
+
+def count_entries_of_nested_list(listOfElem: list) -> int:
+    '''
+    Get number of elements in a nested list
+    '''
     count = 0
-    for elem in listOfElem: # Iterate over the list
-        if type(elem) == list: # Check if type of element is list
-            count += count_entries_of_nested_list(elem) # Recursive call
+    for elem in listOfElem:  # Iterate over the list
+        if type(elem) == list:  # Check if type of element is list
+            count += count_entries_of_nested_list(elem)  # Recursive call
         else:
             count += 1
     return count
 
-def create_unique_list(list_with_duplicates):
+
+def create_unique_list(list_with_duplicates: list) -> list:
     '''
     Takes a list that may contain duplicates and returns a new list with the duplicates removed
     '''
-    #return list(set(list_with_duplicates)) # NOTE: This does not preserve order
+    # return list(set(list_with_duplicates)) # NOTE: This does not preserve order
     return list(dict.fromkeys(list_with_duplicates))
 
-def remove_list_from_list(removal_list, original_list):
+
+def remove_list_from_list(removal_list: list, original_list: list) -> list:
     '''
     Remove items in 'removal_list' if they occur in 'original_list'
     NOTE: This only removes the first occurance of something in a list, care if repeated entries
@@ -159,7 +187,8 @@ def remove_list_from_list(removal_list, original_list):
             new_list.remove(item)
     return new_list
 
-def remove_multiple_elements_from_list(original_list, indices):
+
+def remove_multiple_elements_from_list(original_list: list, indices: list) -> list:
     '''
     Remove multiple elements from a list by providing a list of indices
     '''
@@ -168,7 +197,8 @@ def remove_multiple_elements_from_list(original_list, indices):
         del new_list[index]
     return new_list
 
-def second_largest(numbers):
+
+def second_largest(numbers: list) -> float | int:
     '''
     Returns the second-largest entry in collection of numbers
     '''
@@ -187,27 +217,31 @@ def second_largest(numbers):
 
 ### This set of functions use numpy ###
 
-def is_array_monotonic(x:np.array) -> bool:
+
+def is_array_monotonic(x: np.array) -> bool:
     '''
     Returns True iff the array contains monotonically increasing values
     '''
     return np.all(np.diff(x) > 0.)
 
-def is_array_linear(x:np.array, atol=1e-8) -> bool:
+
+def is_array_linear(x: np.array, atol=1e-8) -> bool:
     '''
     Returns True iff the array is linearly spaced
     '''
     return np.isclose(np.all(np.diff(x)-np.diff(x)[0]), 0., atol=atol)
 
-def arange(min:float, max:float, dtype=None) -> np.ndarray:
+
+def arange(min: float, max: float, dtype=None) -> np.ndarray:
     '''
     Sensible arange function for producing integers from min to max inclusive
     I hate the inbuilt numpy one with such fury that it frightens me
     TODO: Include step properly, what is this ([start,] stop[, step], ...) square-bracket thing?
     '''
-    return np.arange(min, max+1, dtype=dtype)#, like=like)
+    return np.arange(min, max+1, dtype=dtype)  # , like=like)
 
-def linspace_step(start:int, stop:int, step):
+
+def linspace_step(start: int, stop: int, step):
     '''
     Create a linear-spaced array going from start->stop (inclusive) via step
     The end point is included only if it falls exaclty on a step
@@ -217,12 +251,14 @@ def linspace_step(start:int, stop:int, step):
     new_stop = start+(num-1)*step
     return np.linspace(start, new_stop, num)
 
+
 def logspace(xmin, xmax, nx):
     '''
     Return a logarithmically spaced range of numbers
     Numpy version is specifically base10, which is insane since log spacing is independent of base
     '''
     return np.logspace(np.log10(xmin), np.log10(xmax), nx)
+
 
 def is_power_of_n(x, n):
     '''
@@ -231,11 +267,13 @@ def is_power_of_n(x, n):
     lg = np.logn(x, n)
     return is_float_close_to_integer(lg)
 
+
 def is_power_of_two(x):
     '''
     True if argument is a perfect power of two
     '''
     return is_power_of_n(x, 2)
+
 
 def is_perfect_square(x):
     '''
@@ -244,12 +282,14 @@ def is_perfect_square(x):
     root = np.sqrt(x)
     return is_float_close_to_integer(root)
 
+
 def is_perfect_triangle(x):
     '''
     Checks if argument is a perfect triangle number (e.g., 1, 3, 6, 10, ...)
     '''
     n = 0.5*(np.sqrt(1.+8.*x)-1.)
     return is_float_close_to_integer(n)
+
 
 def print_array_attributes(x):
     '''
@@ -262,6 +302,7 @@ def print_array_attributes(x):
     print('dtype:', x.dtype)
     print()
 
+
 def print_full_array(xs, title=None):
     '''
     Print full array(-like) to screen with indices
@@ -272,11 +313,13 @@ def print_full_array(xs, title=None):
     for ix, x in enumerate(xs):
         print(ix, x)
 
+
 def array_of_nans(shape, **kwargs):
     '''
     Initialise an array of nans
     '''
     return np.nan*np.empty(shape, **kwargs)
+
 
 def remove_nans_from_array(x):
     '''
@@ -284,11 +327,13 @@ def remove_nans_from_array(x):
     '''
     return x[~np.isnan(x)]
 
+
 def array_contains_nan(array):
     '''
     Returns True if the array contains any nan values
     '''
     return np.isnan(array.sum())
+
 
 def array_values_at_indices(array, list_of_array_positions):
     '''
@@ -301,8 +346,10 @@ def array_values_at_indices(array, list_of_array_positions):
         result = array[ix, iy]
         return result
     else:
-        ValueError('Error, this only works in either one or two dimensions at the moment')
+        ValueError(
+            'Error, this only works in either one or two dimensions at the moment')
         return None
+
 
 def print_array_statistics(x):
     '''
@@ -327,6 +374,7 @@ def print_array_statistics(x):
     print('<x^2>:', mean**2+var)
     print()
 
+
 def standardize_array(x):
     '''
     From: https://towardsdatascience.com/pca-with-numpy-58917c1d0391
@@ -337,17 +385,19 @@ def standardize_array(x):
     NOTE: In sklearn the 'StandardScaler' exists to do exactly this
     '''
     rows, columns = x.shape
-    
+
     standardizedArray = np.zeros(shape=(rows, columns))
     tempArray = np.zeros(rows)
-    
+
     for col in range(columns):
-        mean = x[:, col].mean(); std = x[:, col].std()
+        mean = x[:, col].mean()
+        std = x[:, col].std()
         tempArray = np.empty(0)
         for element in x[:, col]:
             tempArray = np.append(tempArray, (element-mean)/std)
         standardizedArray[:, col] = tempArray
     return standardizedArray
+
 
 def covariance_matrix(sigmas, R):
     '''
@@ -362,7 +412,8 @@ def covariance_matrix(sigmas, R):
     cov = np.matmul(np.matmul(S, R), S)
     return cov
 
-def find_closest_index_value(x:float, arr:np.array):
+
+def find_closest_index_value(x: float, arr: np.array):
     '''
     Find the index, value pair of the closest values in array 'arr' to value 'x'
     '''
@@ -372,6 +423,7 @@ def find_closest_index_value(x:float, arr:np.array):
 ### ###
 
 ### matplotlib ###
+
 
 def seq_color(i, n, cmap='viridis'):
     '''
@@ -384,11 +436,13 @@ def seq_color(i, n, cmap='viridis'):
         cmap_here = cmap
     return cmap_here(i/(n-1))
 
+
 def colour(i):
     '''
     Default colours (C0, C1, C2, ...)
     '''
-    return 'C%d'%(i)
+    return 'C%d' % (i)
+
 
 def plot_curve_with_error_region(x, y, axis=0, alpha_fill=0.2, label=None, **kwargs):
     '''
